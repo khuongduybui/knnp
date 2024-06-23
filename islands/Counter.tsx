8a6d4ba618735d9e6e044effb995bacc61f6f820
Duy K. Bui <duy@buifamily.info>
2024-06-23 14:10:56 -0400

    feat: attempt to simulate Pixel tactile vibration duration

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -10,7 +10,7 @@ export default function Counter(props: CounterProps) {
     <div class="flex gap-8 py-6">
       <Button vibratePattern={[1]} onClick={() => props.count.value -= 1}>-1</Button>
       <p class="text-3xl tabular-nums">{props.count}</p>
-      <Button vibratePattern={[5]} onClick={() => props.count.value += 1}>+1</Button>
+      <Button vibratePattern={[1]} onClick={() => props.count.value += 1}>+1</Button>
     </div>
   );
 }
