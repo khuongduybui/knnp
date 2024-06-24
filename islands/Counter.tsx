742c6d3a15f8340f2710130c99b01f8242afabc6
Duy K. Bui <duy@buifamily.info>
2024-06-24 01:28:07 -0400

    fix: vibrate

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -6,13 +6,30 @@ interface CounterProps {
 
 export default function Counter(props: CounterProps) {
   return (
-    <div style={{ flexGrow: 1, width: "100vw", display: "flex", alignItems: "center" }} onClick={(_event) => {
-      props.count.value++;
-      window.navigator.vibrate?.([1]);
-      // TODO: Add a sound effect when count mod 10 === 0
-      // TODO: fetch api to increment counter
-    }}>
-      <div style={{ width: "100vw", textAlign: "center", fontSize: "8rem", cursor: "default"}}>{props.count}</div>
+    <div
+      style={{
+        flexGrow: 1,
+        width: "100vw",
+        display: "flex",
+        alignItems: "center",
+      }}
+      onClick={(_event) => {
+        props.count.value++;
+        window.navigator.vibrate([1]);
+        // TODO: Add a sound effect when count mod 10 === 0
+        // TODO: fetch api to increment counter
+      }}
+    >
+      <div
+        style={{
+          width: "100vw",
+          textAlign: "center",
+          fontSize: "8rem",
+          cursor: "default",
+        }}
+      >
+        {props.count}
+      </div>
     </div>
   );
 }
